ad9224cc3d5dc81c28a86e5cb253b23349c73267
jonathan <jonataanthony@gmail.com>
2022-08-06 19:42:19 -0300

    refactor: remove unused imports

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,7 +1,6 @@
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { BadRequest } from "../@shared/errors/BadRequest";
-import { NotFound } from "../@shared/errors/Notfound";
 
 interface IRequest {
   user_id: string;
